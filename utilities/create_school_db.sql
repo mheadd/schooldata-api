6162556dff13fb2fc61e833e16c8f4fa707896a4
JC <jc@ubuntu.(none)>
2013-09-14 13:39:03 -0400

    Added new api calls for radius search, school detail and search by zip

--- utilities/create_school_db.sql
+++ utilities/create_school_db.sql
@@ -135,9 +135,101 @@ BEGIN
 	SELECT school_name_1, school_code, school_level_name, hpaddr, latitude, longitude FROM school_information;
 END$$
 
+DROP PROCEDURE IF EXISTS `schooldata`.`GetSchoolClosingSummary`$$
+
 CREATE PROCEDURE `schooldata`.`GetSchoolClosingSummary`()
 BEGIN
 	SELECT school_name_1, school_code, school_level_name, hpaddr, latitude, longitude FROM school_information WHERE closure ORDER BY school_name_1;
+END$$
+
+DROP PROCEDURE IF EXISTS `schooldata`.`GetSchoolSummaryByZip`$$
+
+CREATE PROCEDURE `schooldata`.`GetSchoolSummaryByZip`(IN postal char(5))
+BEGIN
+	SET @qry = 'SELECT * FROM school_information WHERE school_zip = ?';
+	SET @postal = postal;
+	PREPARE STMT FROM @qry;
+	EXECUTE STMT USING @postal;
+END$$
+
+DROP PROCEDURE IF EXISTS `schooldata`.`GetSchoolDetail`$$
+
+CREATE PROCEDURE `schooldata`.`GetSchoolDetail`(IN code INT)
+BEGIN
+	SET @qry = 'SELECT school_name_1, school_name_2, address, school_zip, zip_plus_4, city, state_cd, phone_number, sch_start_grade, sch_term_grade, hpaddr, school_level_name FROM school_information where school_code = ?';
+	SET @code = code;
+	PREPARE STMT FROM @qry;
+	EXECUTE STMT USING @code;
+END$$
+
+DROP PROCEDURE IF EXISTS `schooldata`.`GetSchoolEthnicity`$$
+
+CREATE PROCEDURE `schooldata`.`GetSchoolEthnicity`(IN code INT)
+BEGIN
+	SET @qry = 'SELECT school_year, african_american, white, asian, latino, other, pacific_islander, american_indian, sch_low_income_family FROM school_ethnicity_low_income WHERE school_code = ?';
+	SET @code = code;
+	PREPARE	STMT FROM @qry;
+	EXECUTE STMT USING @code;
+END$$
+
+DROP PROCEDURE IF EXISTS `schooldata`.`GetSchoolEnrollment`$$
+
+CREATE PROCEDURE `schooldata`.`GetSchoolEnrollment`(IN code INT)
+BEGIN
+	SET @qry = 'SELECT school_year, sch_attendance, sch_enrollment, sch_student_entered, sch_student_withdrew FROM school_enrollment WHERE school_code = ?';
+	SET @code = code;
+	PREPARE	STMT FROM @qry;
+	EXECUTE STMT USING @code;
+END$$
+
+DROP PROCEDURE IF EXISTS `schooldata`.`GetSchoolByRadius`$$
+
+CREATE PROCEDURE `schooldata`.`GetSchoolByRadius`(IN code INT, IN miles INT)
+BEGIN
+	SET @qry = CONCAT('SELECT * , ( 3959 * ACOS( COS( RADIANS( (SELECT latitude from school_information where school_code = ', code, ') ) ) * COS( RADIANS( latitude ) ) * COS( RADIANS( longitude ) - RADIANS( (SELECT longitude from school_information where school_code = ', code, ') ) ) + SIN( RADIANS( (SELECT latitude from school_information where school_code = ', code, ') ) ) * SIN( RADIANS( latitude ) ) ) ) AS distance FROM school_information HAVING distance < ? ORDER BY distance');
+	SET @miles = miles;
+	PREPARE STMT FROM @qry;
+	EXECUTE STMT USING @miles;
+END$$
+
+DROP PROCEDURE IF EXISTS `schooldata`.`GetSchoolStudent`$$
+
+CREATE PROCEDURE `schooldata`.`GetSchoolStudent`(IN code INT)
+BEGIN
+	SET @qry = 'SELECT school_year, sch_spec_ed_services, sch_mg, sch_esol_services FROM school_student WHERE school_code = ?';
+	SET @code  = code;
+	PREPARE STMT FROM @qry;
+	EXECUTE STMT USING @code;
+END$$
+
+DROP PROCEDURE IF EXISTS `schooldata`.`GetSchoolPSSA`$$
+
+CREATE PROCEDURE `schooldata`.`GetSchoolPSSA`(IN code INT)
+BEGIN
+	SET @qry = 'SELECT school_year, grade, math_advanced_percent, math_proficient_percent, math_basic_percent, math_below_basic_percent, read_advanced_percent, read_proficient_percent, read_basic_percent, read_below_basic_percent, math_combined_percent, read_combined_percent FROM school_pssa WHERE school_code = ?';
+	SET @code = code;
+	PREPARE	STMT FROM @qry;
+	EXECUTE STMT USING @code;
+END$$
+
+DROP PROCEDURE IF EXISTS `schooldata`.`GetSchoolIncident`$$
+
+CREATE PROCEDURE `schooldata`.`GetSchoolIncident`(IN code INT)
+BEGIN
+	SET @qry = 'SELECT school_year, assault, drug, morals, weapons, theft FROM school_serious_incidents WHERE school_code = ?';
+	SET @code = code;
+	PREPARE	STMT FROM @qry;
+	EXECUTE STMT USING @code;
+END$$
+
+DROP PROCEDURE IF EXISTS `schooldata`.`GetSchoolSuspension`$$
+
+CREATE PROCEDURE `schooldata`.`GetSchoolSuspension`(IN code INT)
+BEGIN
+	SET @qry = 'SELECT school_year, total_suspensions, sch_one_time_susp, sch_two_time_susp, sch_three_time_susp, sch_more_than_three_susp FROM school_suspensions WHERE school_code = ?';
+	SET @code = code;
+	PREPARE	STMT FROM @qry;
+	EXECUTE STMT USING @code;
 END$$
 
 DELIMITER ;
